64f91d31df30dd1fb2d4f70bfe23b767a383bca0
Diego Viana <diegovianaf7@gmail.com>
2022-06-27 20:22:39 -0300

    add components and styles to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-function App() {
+import GlobalStyle from './Assets/globalStyles'
+import Layout from './Components/Layout/Layout'
+import Profile from './Components/Profile/Profile'
+
+const App = () => {
   return (
-    <div>
-      GitHub User Finder
-    </div>
+    <main>
+      <GlobalStyle />
+      <Layout>
+      <Profile />
+      <section>Repositories</section>
+      <section>Starreds</section>
+      </Layout>
+    </main>
   )
 }
 
