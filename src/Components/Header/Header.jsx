471379dde64592c754ad7ad57a402445db03b31a
Diego Viana <diegovianaf7@gmail.com>
2022-06-30 20:00:11 -0300

    add useState and logic to get user data from the api

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import searchIcon from '../../Assets/icons/search.svg'
 import * as S from './styled'
+import useGithub from '../../Hooks/useGithub'
 
 const Header = () => {
+  const { getUser } = useGithub()
+  const [usernameForSearch, setUsernameForSearch] = useState()
+
+  const submitGetUser = () => {
+    if (!usernameForSearch) return
+    return getUser(usernameForSearch)
+  }
+
   return (
     <header>
       <S.Title>GitHub User Finder</S.Title>
@@ -12,8 +21,9 @@ const Header = () => {
           type="text"
           id="searchInput"
           placeholder="Search GitHub username..."
+          onChange={(event) => setUsernameForSearch(event.target.value)}
         />
-        <button type="submit">Search</button>
+        <button type="submit" onClick={submitGetUser}>Search</button>
       </S.LabelContainer>
     </header>
   )
