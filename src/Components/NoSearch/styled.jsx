96fcf72ddbc8652363be090ee480a6d689900f1d
Diego Viana <diegovianaf7@gmail.com>
2022-07-01 11:54:50 -0300

    add styles to nosearch component

---
+++ src/Components/NoSearch/styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: #fff;
+  border-radius: .5rem;
+  display: flex;
+  justify-content: center;
+  margin: 1rem auto;
+  padding: 2.5rem 2rem;
+  
+  p {
+    font-size: 1.25rem;
+  }
+`
