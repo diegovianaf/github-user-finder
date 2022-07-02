d6dd5ff612b540095179ffc8c0d8607c02fd5e72
Diego Viana <diegovianaf7@gmail.com>
2022-07-02 11:36:22 -0300

    remove unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import GlobalStyle from './Assets/globalStyles'
 import Layout from './Components/Layout/Layout'
 import NoSearch from './Components/NoSearch/NoSearch'
 import Profile from './Components/Profile/Profile'
 import Repositories from './Components/Repositories/Repositories'
 import useGithub from './Hooks/useGithub'
-import GithubProvider from './Providers/GithubProvider'
 
 const App = () => {
   const { githubState } = useGithub()
